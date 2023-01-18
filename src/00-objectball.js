03bc5184caa0bb9cea5643851ed8d1c7e70653ce
Christian Garcia <bones192nd@gmail.com>
2023-01-18 14:31:04 -0500

    built blank object, fill in info

---
+++ src/00-objectball.js
@@ -0,0 +1,121 @@
+function gameObject (){
+    const object = {
+        home : {
+            teamName : "Brooklyn Nets",
+            colors : ["Black", "White"],
+            players : {
+                "name" : {                  // Player 1
+                    number : "",
+                    shoe : "",
+                    points : "",
+                    rebounds : "",
+                    assists : "",
+                    steals : "",
+                    blocks : "",
+                    slamDunks : ""
+                },
+                "name" : {
+                    number : "",
+                    shoe : "",
+                    points : "",
+                    rebounds : "",
+                    assists : "",
+                    steals : "",
+                    blocks : "",
+                    slamDunks : ""
+                },                
+                "name" : {
+                    number : "",
+                    shoe : "",
+                    points : "",
+                    rebounds : "",
+                    assists : "",
+                    steals : "",
+                    blocks : "",
+                    slamDunks : ""
+                },
+                "name" : {
+                    number : "",
+                    shoe : "",
+                    points : "",
+                    rebounds : "",
+                    assists : "",
+                    steals : "",
+                    blocks : "",
+                    slamDunks : ""
+                },
+                "name" : {
+                    number : "",
+                    shoe : "",
+                    points : "",
+                    rebounds : "",
+                    assists : "",
+                    steals : "",
+                    blocks : "",
+                    slamDunks : ""
+                }                                                
+            }
+        },
+        away : {
+            teamName : "Charlotte Hornets",
+            colors : [],
+            players : {
+                "name" : {                  
+                    number : "",
+                    shoe : "",
+                    points : "",
+                    rebounds : "",
+                    assists : "",
+                    steals : "",
+                    blocks : "",
+                    slamDunks : ""
+                },
+                "name" : {
+                    number : "",
+                    shoe : "",
+                    points : "",
+                    rebounds : "",
+                    assists : "",
+                    steals : "",
+                    blocks : "",
+                    slamDunks : ""
+                },                
+                "name" : {
+                    number : "",
+                    shoe : "",
+                    points : "",
+                    rebounds : "",
+                    assists : "",
+                    steals : "",
+                    blocks : "",
+                    slamDunks : ""
+                },
+                "name" : {
+                    number : "",
+                    shoe : "",
+                    points : "",
+                    rebounds : "",
+                    assists : "",
+                    steals : "",
+                    blocks : "",
+                    slamDunks : ""
+                },
+                "name" : {
+                    number : "",
+                    shoe : "",
+                    points : "",
+                    rebounds : "",
+                    assists : "",
+                    steals : "",
+                    blocks : "",
+                    slamDunks : ""
+                }                                                
+            }
+        }
+    }
+
+    return object
+}
+
+console.table(gameObject().home.players)
+
