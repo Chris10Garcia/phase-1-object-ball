8aba98c08657aeed33ea84353893ce78d7269867
Christian Garcia <bones192nd@gmail.com>
2023-01-18 19:58:02 -0500

    added values to data obj table

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -4,55 +4,55 @@ function gameObject (){
             teamName : "Brooklyn Nets",
             colors : ["Black", "White"],
             players : {
-                "name" : {                  // Player 1
-                    number : "",
-                    shoe : "",
-                    points : "",
-                    rebounds : "",
-                    assists : "",
-                    steals : "",
-                    blocks : "",
-                    slamDunks : ""
+                "Alan Anderson" : {                  // Player 1
+                    number : 0,
+                    shoe : 16,
+                    points : 22,
+                    rebounds : 12,
+                    assists : 12,
+                    steals : 3,
+                    blocks : 1,
+                    slamDunks : 1
                 },
-                "name" : {
-                    number : "",
-                    shoe : "",
-                    points : "",
-                    rebounds : "",
-                    assists : "",
-                    steals : "",
-                    blocks : "",
-                    slamDunks : ""
+                "Reggie Evans" : {
+                    number : 30,
+                    shoe : 14,
+                    points : 12,
+                    rebounds : 12,
+                    assists : 12,
+                    steals : 12,
+                    blocks : 12,
+                    slamDunks : 7
                 },                
-                "name" : {
-                    number : "",
-                    shoe : "",
-                    points : "",
-                    rebounds : "",
-                    assists : "",
-                    steals : "",
-                    blocks : "",
-                    slamDunks : ""
+                "Brook Lopez" : {
+                    number : 11,
+                    shoe : 17,
+                    points : 17,
+                    rebounds : 19,
+                    assists : 10,
+                    steals : 3,
+                    blocks : 1,
+                    slamDunks : 15
                 },
-                "name" : {
-                    number : "",
-                    shoe : "",
-                    points : "",
-                    rebounds : "",
-                    assists : "",
-                    steals : "",
-                    blocks : "",
-                    slamDunks : ""
+                "Mason Plumlee" : {
+                    number : 1,
+                    shoe : 19,
+                    points : 26,
+                    rebounds : 12,
+                    assists : 6,
+                    steals : 3,
+                    blocks : 8,
+                    slamDunks : 5
                 },
-                "name" : {
-                    number : "",
-                    shoe : "",
-                    points : "",
-                    rebounds : "",
-                    assists : "",
-                    steals : "",
-                    blocks : "",
-                    slamDunks : ""
+                "Jason Terry" : {
+                    number : 31,
+                    shoe : 15,
+                    points : 19,
+                    rebounds : 2,
+                    assists : 2,
+                    steals : 4,
+                    blocks : 11,
+                    slamDunks : 1
                 }                                                
             }
         },
@@ -60,55 +60,55 @@ function gameObject (){
             teamName : "Charlotte Hornets",
             colors : [],
             players : {
-                "name" : {                  
-                    number : "",
-                    shoe : "",
-                    points : "",
-                    rebounds : "",
-                    assists : "",
-                    steals : "",
-                    blocks : "",
-                    slamDunks : ""
+                "Jeff Adrien" : {                  
+                    number : 4,
+                    shoe : 18,
+                    points : 10,
+                    rebounds : 1,
+                    assists : 1,
+                    steals : 2,
+                    blocks : 7,
+                    slamDunks : 2
                 },
-                "name" : {
-                    number : "",
-                    shoe : "",
-                    points : "",
-                    rebounds : "",
-                    assists : "",
-                    steals : "",
-                    blocks : "",
-                    slamDunks : ""
+                "Bismak Biyombo" : {
+                    number : 0,
+                    shoe : 16,
+                    points : 12,
+                    rebounds : 4,
+                    assists : 7,
+                    steals : 7,
+                    blocks : 15,
+                    slamDunks : 10
                 },                
-                "name" : {
-                    number : "",
-                    shoe : "",
-                    points : "",
-                    rebounds : "",
-                    assists : "",
-                    steals : "",
-                    blocks : "",
-                    slamDunks : ""
+                "DeSagna Diop" : {
+                    number : 2,
+                    shoe : 14,
+                    points : 24,
+                    rebounds : 12,
+                    assists : 12,
+                    steals : 4,
+                    blocks : 5,
+                    slamDunks : 5
                 },
-                "name" : {
-                    number : "",
-                    shoe : "",
-                    points : "",
-                    rebounds : "",
-                    assists : "",
-                    steals : "",
-                    blocks : "",
-                    slamDunks : ""
+                "Ben Gordon" : {
+                    number : 8,
+                    shoe : 15,
+                    points : 33,
+                    rebounds : 3,
+                    assists : 2,
+                    steals : 1,
+                    blocks : 1,
+                    slamDunks : 0
                 },
-                "name" : {
-                    number : "",
-                    shoe : "",
-                    points : "",
-                    rebounds : "",
-                    assists : "",
-                    steals : "",
-                    blocks : "",
-                    slamDunks : ""
+                "Brendan Haywood" : {
+                    number : 33,
+                    shoe : 15,
+                    points : 6,
+                    rebounds : 12,
+                    assists : 12,
+                    steals : 22,
+                    blocks : 5,
+                    slamDunks : 12
                 }                                                
             }
         }
